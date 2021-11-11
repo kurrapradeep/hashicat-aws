54b921dd0e83aa37801816b79bee91fe264039fd
Pradeep Kurra <pradep.kurra@vcoretec.com>
2021-11-11 21:00:05 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/CHIP-Pradeep/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = "PradeepKurra"
 
 
